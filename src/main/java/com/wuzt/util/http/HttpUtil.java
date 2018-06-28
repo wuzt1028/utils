e74a0cf09c0d10a424d5f570430666f5b6491af9
wuzt1028 <wuzt1028@163.com>
2018-06-28 17:53:40 +0800

    1、新增基于httpClient的参数为json串的POST请求util

--- src/main/java/com/wuzt/util/http/HttpUtil.java
+++ src/main/java/com/wuzt/util/http/HttpUtil.java
@@ -14,6 +14,13 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.util.URIUtil;
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -116,5 +123,48 @@ public class HttpUtil {
 	public void doThreadPost(String url, Map<String, String> params) {
 		Thread thread = new Thread(new ThreadHttp(url,params));
 		thread.start();
+	}
+	
+	/**
+	 * 发送json串格式的post请求
+	 * @param url
+	 * @param json
+	 * @return
+	 * 注：maven引用如下<dependency><groupId>org.apache.httpcomponents</groupId><artifactId>httpmime</artifactId><version>4.5.5</version></dependency>
+	 */
+	public String HttpPostWithJson(String url, String json) {
+		String returnValue = "这是默认返回值，接口调用失败";
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		ResponseHandler<String> responseHandler = new BasicResponseHandler();
+		try{
+			//第一步：创建HttpClient对象
+			httpClient = HttpClients.createDefault();
+		 	//第二步：创建httpPost对象
+	        HttpPost httpPost = new HttpPost(url);
+	        //第三步：给httpPost设置JSON格式的参数
+	        StringEntity requestEntity = new StringEntity(json,"utf-8");
+	        requestEntity.setContentEncoding("UTF-8");    	        
+	        httpPost.setHeader("Content-type", "application/json");
+	        httpPost.setEntity(requestEntity);
+	       
+	        //第四步：发送HttpPost请求，获取返回值
+	        returnValue = httpClient.execute(httpPost,responseHandler); //调接口获取返回值时，必须用此方法
+	        CloseableHttpResponse httpResonse = httpClient.execute(httpPost);
+	        int statusCode = httpResonse.getStatusLine().getStatusCode();
+	        if(statusCode!=200){
+	        	returnValue = httpResonse.getStatusLine().toString();	  
+	        }
+		}catch(Exception e){
+			 e.printStackTrace();
+		}finally {
+	    	try {
+	    		httpClient.close();
+	    	} catch (IOException e) {
+	    		// TODO Auto-generated catch block
+	    		e.printStackTrace();
+	    	}
+	    }
+		 //第五步：处理返回值
+	     return returnValue;
 	}
 }
